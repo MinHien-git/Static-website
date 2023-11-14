8ecb71e970bd63e2a4437bab760af92178a7c7c5
duck420i69 <88267139+duck420i69@users.noreply.github.com>
2023-11-14 17:28:26 +0700

    add navigation for phan cum phuong quan

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -5,3 +5,13 @@ let primary_navigation_bar = $("#primary-navigation-bar");
 burger.on("click", (e) => {
   primary_header.toggleClass("active");
 });
+
+let navbar = document.querySelectorAll("#primary-items-list li");
+navbar.forEach((e) => {
+    e.addEventListener("mouseover", () => { 
+        e.style.color = "hsl(219, 90%, 69%)";
+    });
+    e.addEventListener("mouseout", () => {
+        e.style.color = "black";
+    })
+})
