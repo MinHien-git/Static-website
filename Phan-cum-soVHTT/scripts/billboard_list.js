bce4ae998622af5b6c2599889b474a4c6ff03e5e
Huy <mana27112002@gmail.com>
2023-11-18 14:25:36 +0700

    added popups on QuanLiQC and added DuyetYCCapPhep page

--- Phan-cum-soVHTT/scripts/billboard_list.js
+++ Phan-cum-soVHTT/scripts/billboard_list.js
@@ -1,6 +1,6 @@
 //toggle collapse button
-function toggleCollapseButton() {
-	const btn = document.getElementById("collapse__btn")
+function toggleCollapseButton(btn_id) {
+	const btn = document.getElementById(btn_id)
 	const icon = btn.querySelector("i")
 	if (btn.classList.contains("collapsed")) {
 		icon.classList.remove("bi-chevron-up")
@@ -9,4 +9,21 @@ function toggleCollapseButton() {
 		icon.classList.add("bi-chevron-up")
 		icon.classList.remove("bi-chevron-down")
 	}
+}
+
+function showToast(toast_id) {
+	const toast_element = document.getElementById(toast_id)
+	let toast_item = new bootstrap.Toast(toast_element)
+	toast_item.show()
+	console.log(toast_element)
+}
+
+function toggleTextarea(checkbox_id, textarea_id) {
+	const textarea_el = document.getElementById(textarea_id)
+	const checkbox_el = document.getElementById(checkbox_id)
+	console.log(textarea_el)
+	console.log(checkbox_el)
+	if (checkbox_el.checked) {
+		textarea_el.style.display = "flex"
+	} else textarea_el.style.display = "none"
 }
